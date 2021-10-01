970804eb33d50e8c5bc9900f198c331b8f779c6b
Phoenix661T <thabangmaloka661@outlook.com>
2021-10-01 16:14:16 -0700

    main.h file

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_HF
+#define MAIN_HF
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define ABS(x) ((x) < (0) ? (-(x)) : (x))
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list p);
+int print_string(va_list p);
+int print_int(va_list p);
+int print_rot13(va_list p);
+int print_binary(va_list p);
+int print_rev_str(va_list p);
+int _pow(int x, int y);
+void rev_string(char *s);
+/**
+ * struct list - a list of types of arguments.
+ * @c: First member
+ * @f: Second member pointer to function
+ *
+ * Description: a list of types of arguments passed to the function
+ */
+typedef struct list
+{
+  char c;
+  int (*f)(va_list);
+} list;
+int (*get_op_func(char s))(va_list);
+#endif
