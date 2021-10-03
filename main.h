bac8d62ecc3bbf3d7c4631d7cbce592bb82bafc5
Phoenix661T <thabangmaloka661@outlook.com>
2021-10-03 13:17:50 -0700

    printf

--- main.h
+++ main.h
@@ -1,30 +1,46 @@
-#ifndef MAIN_HF
-#define MAIN_HF
-#include <stdarg.h>
-#include <stdlib.h>
+#ifndef PRINT_F
+#define PRINT_F
+
 #include <unistd.h>
-#define ABS(x) ((x) < (0) ? (-(x)) : (x))
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_char(va_list p);
-int print_string(va_list p);
-int print_int(va_list p);
-int print_rot13(va_list p);
-int print_binary(va_list p);
-int print_rev_str(va_list p);
-int _pow(int x, int y);
-void rev_string(char *s);
+#include <stdlib.h>
+#include <stdarg.h>
+
 /**
- * struct list - a list of types of arguments.
- * @c: First member
- * @f: Second member pointer to function
+ * struct convert - defines a structure for symbols and functions
  *
- * Description: a list of types of arguments passed to the function
+ * @sym: The operator
+ * @f: The function associated
  */
-typedef struct list
+struct convert
 {
-  char c;
+  char *sym;
   int (*f)(va_list);
-} list;
-int (*get_op_func(char s))(va_list);
+};
+typedef struct convert conver_t;
+
+/*Main functions*/
+int parser(const char *format, conver_t f_list[], va_list arg_list);
+int _printf(const char *format, ...);
+int _write_char(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
+
+/*Helper functions*/
+unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
+void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsgined_number(unsigned int);
+
+
 #endif
